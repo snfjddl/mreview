e1913f1f03c42a4244040ed052dfed45a5a848a9
snfjddl <seokil1220@gmail.com>
2022-01-13 16:05:28 +0900

    영화 목록 기능 및 화면 구현

--- src/main/java/org/zerock/mreview/controller/MovieController.java
+++ src/main/java/org/zerock/mreview/controller/MovieController.java
@@ -3,11 +3,13 @@ package org.zerock.mreview.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.mreview.dto.MovieDTO;
+import org.zerock.mreview.dto.PageRequestDTO;
 import org.zerock.mreview.service.MovieService;
 
 @Controller
@@ -33,4 +35,13 @@ public class MovieController {
 
         return "redirect:/movie/list";
     }
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO requestDTO, Model model) {
+        log.info("pageRequestDTO: " + requestDTO);
+
+        model.addAttribute("result", movieService.getList(requestDTO));
+
+    }
+
 }
--- src/main/java/org/zerock/mreview/dto/MovieDTO.java
+++ src/main/java/org/zerock/mreview/dto/MovieDTO.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,4 +21,8 @@ public class MovieDTO {
     @Builder.Default
     private List<MovieImageDTO> imageDTOList = new ArrayList<>();
 
+    private Double avg;    // 평균 평점
+    private Long reviewCount;
+    private LocalDateTime regDate;
+    private LocalDateTime modDate;
 }
