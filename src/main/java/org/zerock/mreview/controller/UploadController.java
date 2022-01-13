4819f308830079127f3cb2f27d1610aa255784eb
snfjddl <seokil1220@gmail.com>
2022-01-13 20:53:40 +0900

    영화 리뷰 기능 및 화면 완성

--- src/main/java/org/zerock/mreview/controller/UploadController.java
+++ src/main/java/org/zerock/mreview/controller/UploadController.java
@@ -100,7 +100,7 @@ public class UploadController {
     }
 
     @GetMapping("/display")
-    public ResponseEntity<byte[]> getFile(String fileName) {
+    public ResponseEntity<byte[]> getFile(String fileName, String size) {
 
         ResponseEntity<byte[]> result = null;
 
@@ -110,6 +110,11 @@ public class UploadController {
             log.info("fileName: " + srcFileName);
 
             File file = new File(uploadPath + File.separator + srcFileName);
+            log.info("file: " + file);
+
+            if (size != null && size.equals("1")) {
+                file = new File(file.getParent(), file.getName().substring(2));
+            }
             log.info("file: " + file);
 
             HttpHeaders header = new HttpHeaders();
